7a1b41923d6fa7249e5319d50446525facb059b8
Hout Angkeara <houtankeara017@gmail.com>
2026-08-12 14:43:26 +0700

    Use default secret for JWT token generation

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,7 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+  const secret = process.env.JWT_SECRET || "1d8dbfdb1328bb68cb413ab6c8d4ca33dd1db3c7bff405e5cb62b26d87332fef";
+
+  return jwt.sign({ id }, secret, {
     expiresIn: "7d",
   });
 };
